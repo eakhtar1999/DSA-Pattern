e64b22b5c0ea24f4b74b573f00678207effb6618
Ehtesham Akhtar <54777668+eakhtar1999@users.noreply.github.com>
2026-05-11 10:36:29 +0530

    Refine comments in Container With Most Water
    Updated comments to clarify problem statement and constraints.

---
+++ two-pointer/3-L11-Container With Most Water.java
@@ -0,0 +1,27 @@
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+// Return the maximum amount of water a container can store.
+// Notice that you may not slant the container.
+
+
+// TC: O(n)
+// SC: O(1)
+
+public class Solution {
+    public int maxArea(int[] heights) {
+        int l = 0;
+        int r = heights.length - 1;
+        int res = 0;
+
+        while (l < r) {
+            int area = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, area);
+            if (heights[l] <= heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
